f655c4fada22e35e7ae174665d83660b105921ae
Giochxa <151730615+Giochxa@users.noreply.github.com>
2026-05-19 00:34:39 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-using LiteDB;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using PunchServerMVC.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<PunchRepository>();
-builder.Services.AddSingleton<IRepository, PunchRepository>();
-// builder.Services.AddSingleton<IOrganisationRepository, OrganisationRepository>();
-
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-=======
 using LiteDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -77,4 +41,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
->>>>>>> master
